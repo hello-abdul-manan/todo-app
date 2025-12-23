f1259dc1c28e8c58e6061fc7efe53394e9008765
hello-abdul-manan <helloabdulmanan@gmail.com>
2025-12-23 22:53:24 +0500

    Add a Todo model

--- todo_web_app/todo/models.py
+++ todo_web_app/todo/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Todo(models.Model):
+    CHOICES = [
+        ('L', 'Low'),
+        ('M', 'Medium'),
+        ('H', 'High'),
+    ]
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='todos')
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=1, choices=CHOICES, default='M')
+    due_date = models.DateField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+    
