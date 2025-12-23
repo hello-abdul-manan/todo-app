d8777802b72cccfddc519ce930573b2fc2c99a67
hello-abdul-manan <helloabdulmanan@gmail.com>
2025-12-24 01:04:29 +0500

    Create a view for creating todos

--- todo_web_app/todo/views.py
+++ todo_web_app/todo/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
+from .forms import TodoForm
 
 # Create your views here.
 def todo_list(request):
     todos = Todo.objects.filter(user=request.user).order_by('-created_at')
     return render(request, 'todo/todo_list.html', {'todos': todos})
+
+def todo_create(request):
+    if request.method == 'POST':
+        form = TodoForm(request.POST)
+        if form.is_valid():
+            todo = form.save(commit=False)
+            todo.user = request.user
+            form.save()
+            return redirect('todo_list')
+    else:
+        form = TodoForm()
+    return render(request, 'todo/todo_create.html', {'form': form, 'title': 'Create Todo'})
