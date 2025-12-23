11e1ca187088f7e1fd328db89f2433dcdb92ec0c
hello-abdul-manan <helloabdulmanan@gmail.com>
2025-12-23 23:20:10 +0500

    Register Todo model

--- todo_web_app/todo/admin.py
+++ todo_web_app/todo/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Todo
 
 # Register your models here.
+@admin.register(Todo)
+class TodoAdmin(admin.ModelAdmin):
+    list_display = ('user', 'title', 'description', 'completed', 'priority', 'due_date', 'created_at', 'updated_at')
+    list_filter = ('completed', 'priority', 'user')
+    search_fields = ('title',)
+    ordering = ('-created_at',)
