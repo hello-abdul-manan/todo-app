f8b4b8f7ef93575c0ca9c646e48f7ef00d9ac1e9
hello-abdul-manan <helloabdulmanan@gmail.com>
2025-12-24 13:02:56 +0500

    Create urls for the app

--- todo_web_app/todo/urls.py
+++ todo_web_app/todo/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.todo_list, name='todo_list'),
+    path('', views.landing_page, name='landing'),
+    path('list/', views.todo_list, name='todo_list'),
+    path('create/', views.todo_create, name='todo_create'),
+    path('update/<int:todo_id>/', views.todo_update, name='todo_update'),
+    path('delete/<int:todo_id>/', views.todo_delete, name='todo_delete'),
+
+    # Authentication
+    path('signup/', views.signup_view, name='signup'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
 ]
