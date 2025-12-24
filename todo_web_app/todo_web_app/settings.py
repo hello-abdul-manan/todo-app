69852e47d452b70993b3b7bba01e1807edfd0a1c
hello-abdul-manan <helloabdulmanan@gmail.com>
2025-12-24 13:03:40 +0500

    Customize urls for user registration

--- todo_web_app/todo_web_app/settings.py
+++ todo_web_app/todo_web_app/settings.py
@@ -117,3 +117,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
+
+LOGIN_URL = '/todos/login/'
+LOGIN_REDIRECT_URL = '/todos/list/'
+LOGOUT_REDIRECT_URL = '/todos/'
